8b4249dbbc6ac88a9fd8231c9d57432ed8b20b11
tobi208 <tobi208@github.com>
2022-03-29 13:48:45 +0200

    adjusted horizon plot to consider taxonomy abundances in relation to each other

--- desktop/src/mmonitor/calculations/horizon_r.py
+++ desktop/src/mmonitor/calculations/horizon_r.py
@@ -29,15 +29,24 @@ def generate_image(df: pd.DataFrame, width: int, height: int) -> None:
              - Find a way to generate horizon plots in Plotly
     """
 
-    _write_csv(df)
-    _export_env_vars(width, height)
+    horizon_scale = _write_csv(df)
+    _export_env_vars(width, height, horizon_scale)
     _call_r_script()
 
 
-def _write_csv(df: pd.DataFrame) -> None:
+def _write_csv(df: pd.DataFrame) -> int:
     """
     Transpose mmonitor db to a Dataframe with only taxonomies and their abundances.
     """
+
+    # calculate the width of bands for the horizon plot assuming 6 bands
+    horizon_scale = (df['abundance'].max() - df['abundance'].min()) // 6
+
+    # scale the abundances so that 0 is the origin for a better y axis label
+    def shift(x):
+        return x - horizon_scale * 3
+
+    df['abundance'] = df['abundance'].apply(shift)
 
     # target df
     csv_df = pd.DataFrame()
@@ -54,8 +63,10 @@ def _write_csv(df: pd.DataFrame) -> None:
     # write file
     csv_df.to_csv(_CSV_FILE, index=False)
 
+    return horizon_scale
 
-def _export_env_vars(width: int, height: int) -> None:
+
+def _export_env_vars(width: int, height: int, horizon_scale: int) -> None:
     """
     Export necessary variables to environment
     so that the R script can read them from the environment.
@@ -63,6 +74,7 @@ def _export_env_vars(width: int, height: int) -> None:
 
     environ['HORI_WIDTH'] = str(width)
     environ['HORI_HEIGHT'] = str(height)
+    environ['HORI_BANDWIDTH'] = str(horizon_scale)
     environ['HORI_CSV'] = _CSV_FILE
     environ['HORI_IMAGE'] = _IMAGE_FILE
 
--- desktop/src/resources/r/horizon.r
+++ desktop/src/resources/r/horizon.r
@@ -1,10 +1,12 @@
 abundances <- read.csv(file = Sys.getenv("HORI_CSV"))
 width <- strtoi(Sys.getenv("HORI_WIDTH"))
 height <- strtoi(Sys.getenv("HORI_HEIGHT"))
+bandwidth <- strtoi(Sys.getenv("HORI_BANDWIDTH"))
 
 library("latticeExtra")
 png(Sys.getenv("HORI_IMAGE"), width = width, height = height)
 horizonplot(ts(abundances), colorkey = TRUE, layout = c(1, ncol(abundances)),
-            strip.left = FALSE, xlab = "Sample ID", ylab = list(rev(colnames(abundances)), rot = 0, cex = 1.35))
+            strip.left = FALSE, xlab = "Sample ID", ylab = list(rev(colnames(abundances)), rot = 0, cex = 1.35),
+            horizonscale = bandwidth, origin = 0)
 
 dev.off()
