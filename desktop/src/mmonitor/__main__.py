bbfc468ff0d7197bc16ab3c7e4bdd7cd65882504
lucass122 <timolucas1994@gmail.com>
2023-11-10 14:41:40 +0100

    updated path environmental variable in __main__.py

--- desktop/src/mmonitor/__main__.py
+++ desktop/src/mmonitor/__main__.py
@@ -1,7 +1,15 @@
+import os
 import sys
 
 from build_mmonitor_pyinstaller import ROOT
 from mmonitor.userside.view import GUI
+
+# set up PYTHONPATH correctly, when running __main__.py
+
+base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if base_path not in sys.path:
+    sys.path.insert(0, base_path)
+
 
 print(ROOT)
 print(f"Before adding folder to sys.path: {sys.path}")
