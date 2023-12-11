cf8042aa35dfc7996c615650b0f2a1ce4d4f5897
lucass122 <timolucas1994@gmail.com>
2023-12-11 12:03:56 +0100

    changed bug with boolean user params

--- desktop/src/mmonitor/userside/MMonitorCMD.py
+++ desktop/src/mmonitor/userside/MMonitorCMD.py
@@ -40,10 +40,10 @@ class MMonitorCMD:
         parser.add_argument("--overwrite", action="store_true",
                             help="Enable to overwrite existing records. If not specified, defaults to False.")
 
-        parser.add_argument('-q', '--qc', action="store_ture",
+        parser.add_argument('-q', '--qc', action="store_true",
                             help='Calculate quality control statistics for input samples.'
                                                           ' Enable this to fill QC app with data. Increases time the analysis takes.')
-        parser.add_argument('-x', '--update', action="store_ture", help='Only update counts and abundances'
+        parser.add_argument('-x', '--update', action="store_true", help='Only update counts and abundances'
                                                               ' Enable this if you want to send count and abundances to the MMonitor webserver DB. '
                                                               ' Can be useful, e.g. when the tsv files from emu changed after analysis.')
         parser.add_argument('-n', '--minabundance', type=float, default=0.5,
